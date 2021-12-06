e42fd0764da8ea25a8660b6f5b92baebb274ba0a
JamieShifter <54035117+JamieShifter@users.noreply.github.com>
2021-12-06 02:56:21 +0100

    Add files via upload
    index + css + js initial upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log("Witam!");
+
+let send = document.querySelector(".send-button");
+
+send.addEventListener("click", () => {
+    alert("Dzięki za kliknięcie!");
+})
+
+function smaller() {
+    document.body.style.fontSize = "smaller";
+}
+
+function bigger() {
+    document.body.style.fontSize = "larger";
+}
+
+console.log(przycisk)
