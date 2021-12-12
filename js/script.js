cc3e9986e0dda77b375a4f1fa59bee3a11098c5c
JamieShifter <54035117+JamieShifter@users.noreply.github.com>
2021-12-12 17:29:54 +0100

    Add files via upload
    * Updated layout
    * Added navbar
    * Content centered
    * Photo turned radial
    * Switched to google fonts

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 console.log("Witam!");
 
-let send = document.querySelector(".send-button");
+let send = document.querySelector(".sendButton");
+let button = document.querySelector(".displayMode");
+let body = document.querySelector(".body");
 
 send.addEventListener("click", () => {
     alert("Dzięki za kliknięcie!");
+});
+
+button.addEventListener("click", () => {
+    if (body.className === "body" && button.textContent === "Dark Mode") {
+        body.className = "darkmode";
+        button.textContent = "Light Mode";
+    } else {
+        body.className = "body";
+        button.textContent = "Dark Mode";
+    }
 })
 
-function smaller() {
-    document.body.style.fontSize = "smaller";
-}
-
-function bigger() {
-    document.body.style.fontSize = "larger";
-}
 
